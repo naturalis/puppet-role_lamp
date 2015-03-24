d5952c3f83d199b46ccdf269ccbd60a7cd6f98a7
Foppe Pieters <foppe.pieters@naturalis.nl>
2015-03-24 11:30:08 +0100

    cleaned up

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,81 +7,82 @@
 #
 #
 class role_lamp (
-  $docroot                      = '/var/www/htdocs',
-  $webdirs                      = ['/var/www/htdocs'],
-  $rwwebdirs                    = ['/var/www/htdocs/cache'],
-  $enable_mysql                 = undef,
-  $enable_phpmyadmin            = false,
-  $mysql_root_password          = 'rootpassword',
-  $mysql_manage_config_file     = 'true',
-  $mysql_key_buffer_size        = undef,
-  $mysql_query_cache_limit      = undef,
-  $mysql_query_cache_size       = undef,
-  $mysql_innodb_buffer_pool_size = undef,
-  $mysql_innodb_additional_mem_pool_size = undef,
-  $mysql_innodb_log_buffer_size = undef,
-  $mysql_max_connections        = undef,
-  $mysql_max_heap_table_size    = undef,
-  $instances                    = {'site.lampsite.nl' => {
-                           'serveraliases'   => '*.lampsite.nl',
-                           'docroot'         => '/var/www/htdocs',
-                           'directories'     => [{ 'path' => '/var/www/htdocs', 'options' => '-Indexes +FollowSymLinks +MultiViews', 'allow_override' => 'All' }],
-                           'port'            => 80,
-                           'serveradmin'     => 'webmaster@naturalis.nl',
-                           'priority'        => 10,
-                          },
-                         },
-  $keepalive                  = 'Off',
-  $max_keepalive_requests     = '100', 
-  $keepalive_timeout          = '15',
+  $docroot                                = '/var/www/htdocs',
+  $webdirs                                = ['/var/www/htdocs'],
+  $rwwebdirs                              = ['/var/www/htdocs/cache'],
+  $enable_mysql                           = undef,
+  $enable_phpmyadmin                      = false,
+  $mysql_root_password                    = 'rootpassword',
+  $mysql_manage_config_file               = true,
+  $mysql_key_buffer_size                  = undef,
+  $mysql_query_cache_limit                = undef,
+  $mysql_query_cache_size                 = undef,
+  $mysql_innodb_buffer_pool_size          = undef,
+  $mysql_innodb_additional_mem_pool_size  = undef,
+  $mysql_innodb_log_buffer_size           = undef,
+  $mysql_max_connections                  = undef,
+  $mysql_max_heap_table_size              = undef,
+  $instances                              =
+          {'site.lampsite.nl' => {
+            'serveraliases'   => '*.lampsite.nl',
+            'docroot'         => '/var/www/htdocs',
+            'directories'     => [{ 'path' => '/var/www/htdocs', 'options' => '-Indexes +FollowSymLinks +MultiViews', 'allow_override' => 'All' }],
+            'port'            => 80,
+            'serveradmin'     => 'webmaster@naturalis.nl',
+            'priority'        => 10,
+            },
+          },
+  $keepalive                            = 'Off',
+  $max_keepalive_requests               = '100',
+  $keepalive_timeout                    = '15',
 ){
 
     file { $webdirs:
-      ensure         => 'directory',
-      mode           => '0750',
-      owner          => 'root',
-      group          => 'www-data',
-      require        => Class['apache']
+      ensure                  => 'directory',
+      mode                    => '0750',
+      owner                   => 'root',
+      group                   => 'www-data',
+      require                 => Class['apache']
     }->
     file { $rwwebdirs:
-      ensure         => 'directory',
-      mode           => '0777',
-      owner          => 'www-data',
-      group          => 'www-data',
-      require        => File[$webdirs]
+      ensure                  => 'directory',
+      mode                    => '0777',
+      owner                   => 'www-data',
+      group                   => 'www-data',
+      require                 => File[$webdirs]
     }
-  
+
 # install php module php-gd
   php::module { [ 'gd','mysql','curl' ]: }
 
 # Install apache and enable modules
   class { 'apache':
-    default_mods     => true,
-    mpm_module       => 'prefork',
-    keepalive              => $keepalive,
-    max_keepalive_requests => $max_keepalive_requests,
-    keepalive_timeout      => $keepalive_timeout,
+    default_mods              => true,
+    mpm_module                => 'prefork',
+    keepalive                 => $keepalive,
+    max_keepalive_requests    => $max_keepalive_requests,
+    keepalive_timeout         => $keepalive_timeout,
   }
-  
+
   include apache::mod::php
   include apache::mod::rewrite
   include apache::mod::speling
 
 # Create instances (vhosts)
-  class { 'role_lamp::instances': 
-      instances      => $instances,
+  class { 'role_lamp::instances':
+      instances               => $instances,
   }
 
 # Configure MySQL Security and finetuning if needed
   if $enable_mysql {
     class { 'mysql::server::account_security':}
     class { 'mysql::server':
-        root_password      => $mysql_root_password,
-        service_enabled    => true,
-        service_manage     => true,
-        manage_config_file => $mysql_manage_config_file,
-        override_options   => {
-          'mysqld' => {
+        root_password         => $mysql_root_password,
+        service_enabled       => true,
+        service_manage        => true,
+        manage_config_file    => $mysql_manage_config_file,
+        override_options      => {
+          'mysqld'            => {
             'key_buffer_size'                 => $mysql_key_buffer_size,
             'query_cache_limit'               => $mysql_query_cache_limit,
             'query_cache_size'                => $mysql_query_cache_size,
@@ -97,23 +98,23 @@ class role_lamp (
 # Configure phpMyadmin
   if $enable_phpmyadmin {
     package { 'phpmyadmin':
-      ensure            => "installed",
-      require           => Package['apache2'],
-      notify            => Exec['link-phpmyadmin', 'enable-mcrypt'],
+      ensure                  => 'installed',
+      require                 => Package['apache2'],
+      notify                  => Exec['link-phpmyadmin', 'enable-mcrypt'],
     }
-    exec { "link-phpmyadmin":
-      command           => "ln -sf /usr/share/phpmyadmin ${webdirs}/phpmyadmin",
-      path              => ["/bin"],
-      require           => File[$webdirs],
-      refreshonly       => true,
+    exec { 'link-phpmyadmin':
+      command                 => "ln -sf /usr/share/phpmyadmin ${webdirs}/phpmyadmin",
+      path                    => ['/bin'],
+      require                 => File[$webdirs],
+      refreshonly             => true,
     }
     exec { 'enable-mcrypt':
-    command             => 'php5enmod mcrypt',
-    path                => ['/bin', '/usr/bin', '/usr/sbin'],
-    require             => Package['phpmyadmin', 'apache2'],
-    refreshonly         => true,
-    notify              => Service['apache2'],
+    command                   => 'php5enmod mcrypt',
+    path                      => ['/bin', '/usr/bin', '/usr/sbin'],
+    require                   => Package['phpmyadmin', 'apache2'],
+    refreshonly               => true,
+    notify                    => Service['apache2'],
   }
   }
-  
+
 }
